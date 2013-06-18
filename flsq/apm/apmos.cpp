168a0d943c18c4f93899fc001e7f35f2900faa50
Kevin Hester <kevinh@geeksville.com>
2013-06-17 17:44:02 -0700

    add sending glue for apm

--- flsq/apm/apmos.cpp
+++ flsq/apm/apmos.cpp
@@ -42,6 +42,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <mqueue.h>
+#include <fcntl.h>
 
 #include <squirrel.h>
 #include <sqstdblob.h>
@@ -54,12 +56,103 @@
 #include "apmos.hpp"
 
 
+struct MsgInfo {
+  uint8_t *bytes;
+  int     numBytes;
+};
+
+mqd_t fromAPM = NULL;
+
+uint8_t outbuf[128], *writep;
+int numWritten;
+
+__EXPORT void flsq_send_start(void) {
+  if(!fromAPM) {
+
+    printf("sender_thread: Starting\n");
+
+    /* Fill in attributes for message queue */
+
+    struct mq_attr attr;
+    attr.mq_maxmsg  = 1; // FIXME - support multiple messages in flight
+    attr.mq_msgsize = sizeof(MsgInfo);
+    attr.mq_flags   = 0;
+
+    /* Set the flags for the open of the queue.
+     * Make it a blocking open on the queue, meaning it will block if
+     * this process tries to send to the queue and the queue is full.
+     *
+     *   O_CREAT - the queue will get created if it does not already exist.
+     *   O_WRONLY - we are only planning to write to the queue.
+     *
+     * Open the queue, and create it if the receiving process hasn't
+     * already created it.
+     */
+
+    fromAPM = mq_open("FrAPM", O_WRONLY|O_CREAT|O_NONBLOCK, 0666, &attr);
+  }
+
+  writep = outbuf;
+  numWritten = 0;
+}
+
+__EXPORT void flsq_send_end(void) {
+  MsgInfo msg;
+  msg.bytes = outbuf;
+  msg.numBytes = numWritten;
+  int status = mq_send(fromAPM, &msg, sizeof(msg), 42);
+  // FIXME - support multiple messages in flight, don't just drop things
+}
+
+__EXPORT void flsq_send_buffer(const uint8_t *buf, uint8_t len) {
+  // Crude check for maxsize
+  if(numWritten + len <= sizeof(outbuf)) {
+    memcpy(writep, buf, len);
+    numWritten += len;
+    writep += len;
+  }
+}
+
+APMOS::APMOS() {
+
+  printf("receiver_thread: Starting\n");
+
+  /* Fill in attributes for message queue */
+
+  struct mq_attr attr;
+  attr.mq_maxmsg  = 20;
+  attr.mq_msgsize = sizeof(MsgInfo);
+  attr.mq_flags   = 0;
+
+  /* Set the flags for the open of the queue.
+   * Make it a blocking open on the queue, meaning it will block if
+   * this task tries to read from the queue when the queue is empty
+   *
+   *   O_CREAT - the queue will get created if it does not already exist.
+   *   O_RDONLY - we are only planning to read from the queue.
+   *
+   * Open the queue, and create it if the sending process hasn't
+   * already created it.
+   */
+
+  incoming = mq_open("FrAPM", O_RDONLY|O_CREAT, 0666, &attr);
+}
+
 void APMOS::sendMavlink(const uint8_t *payload, unsigned payloadLen) {
 }
 
 
 
 bool APMOS::waitMessage(int waitMsecs, uint8_t *destBuf, int *destBufLen) {
-	// FIXME
-	return false;
+  MsgInfo msg;
+
+  // FIXME - use the specified timeout wiht timedreceive
+  int nbytes = mq_receive(incoming, &msg, sizeof(msg), 0);
+  if(nbytes > 0) {
+    memcpy(destBuf, msg.bytes, msg.numBytes); // FIXME validate max len
+    *destBufLen = msg.numBytes;
+    return true;
+  }
+  else
+    return false;
 }
--- flsq/apm/apmos.hpp
+++ flsq/apm/apmos.hpp
@@ -1,14 +1,20 @@
 #ifndef __APMOS_HPP
 #define __APMOS_HPP
 
+#include <mqueue.h>
+
 #include "SQOS.hpp"
 
 class APMOS : public SQOS {
-protected:
-	// Send a message
-    virtual void sendMavlink(const uint8_t *payload, unsigned payloadLen);
+  mqd_t incoming;
 
-	virtual bool waitMessage(int waitMsecs, uint8_t *destBuf, int *destBufLen);
+  APMOS();
+
+protected:
+  // Send a message
+  virtual void sendMavlink(const uint8_t *payload, unsigned payloadLen);
+
+  virtual bool waitMessage(int waitMsecs, uint8_t *destBuf, int *destBufLen);
 
 };
 
